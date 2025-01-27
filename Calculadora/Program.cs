d4e02a6edf267309f1b9884e41d6f55a6aa477ea
Isaac Hasselmann <isaacbilac920@gmail.com>
2025-01-27 15:20:36 -0300

    Lab_Testes_Unitarios

---
+++ Calculadora/Program.cs
@@ -0,0 +1,8 @@
+﻿using Calculadora.Services;
+
+CalculadoraImp c = new CalculadoraImp();
+
+int num1 = 10;
+int num2 = 20;
+
+Console.WriteLine($"A soma de {num1} e {num2} é : {c.Somar(num1, num2)}");
